2eeba7701b5021004f28308c9596abea79242b9a
Loki <Loki.Astari@gmail.com>
2020-03-09 07:57:41 -0700

    Fix Hot Spot
        The draw() method can be called anytime.
        So the state of the Walkers is not in a specific defined state.
    
        Move all the code that depends on the state of the walkers into
        tick() as this only happens after the Walkeers have been sorted.

--- src/Walker/DrawableDistanceHotMap.cpp
+++ src/Walker/DrawableDistanceHotMap.cpp
@@ -16,39 +16,6 @@ void DrawableDistanceHotMap::draw(wxDC& dc) const
     dc.DrawRectangle(wxPoint{0,0}, size);
 
     dc.SetPen(*wxBLACK_PEN);
-
-
-    int     minBucketWidth  = 10;
-    int     maxBucketCount  = (size.x - 20) / minBucketWidth;
-
-    int     maxValue        = std::max(0, walkers[0].score() + 1);
-    int     minValue        = std::min(0, walkers[99].score() - 1);
-    float   bucketRange     = (maxValue - minValue) * 1.0 / maxBucketCount;
-    int     posBucketCount  = (maxValue / bucketRange ) + 1;
-    int     negBucketCount  = (-minValue / bucketRange) + 1;
-    int     totalCount      = posBucketCount + negBucketCount;
-    int     maxCount        = 0;
-
-    std::vector<int>    count(totalCount, 0);
-    for (auto const& walker: walkers)
-    {
-        int bucketIndex;
-        int score = walker.score();
-
-        if (score < 0)
-        {
-            bucketIndex = negBucketCount - 1 - (-score / bucketRange);
-        }
-        else
-        {
-            bucketIndex = negBucketCount + score / bucketRange;
-        }
-        ++count[bucketIndex];
-        maxCount = std::max(maxCount, count[bucketIndex]);
-    }
-
-    float   distYRel    = (size.y - 20) * 1.0 / maxCount;
-
     for (std::size_t loop = 0; loop < count.size(); ++loop)
     {
         int xCord   = 10 + loop * 10;
@@ -66,6 +33,40 @@ extern wxPanel* panelDistanceMap;
 
 void DrawableDistanceHotMap::tick(bool update)
 {
+    wxSize size = getSize();
+    int     minBucketWidth  = 10;
+    int     maxBucketCount  = (size.x - 20) / minBucketWidth;
+
+    int     maxValue        = std::max(0, walkers[0].score() + 1);
+    int     minValue        = std::min(0, walkers[99].score() - 1);
+    float   bucketRange     = (maxValue - minValue) * 1.0 / maxBucketCount;
+    int     posBucketCount  = (maxValue / bucketRange ) + 1;
+    int     negBucketCount  = (-minValue / bucketRange) + 1;
+    int     totalCount      = posBucketCount + negBucketCount;
+    int     maxCount        = 1;
+
+    count.clear();
+    count.resize(totalCount, 0);
+
+    for (auto const& walker: walkers)
+    {
+        int bucketIndex;
+        int score = walker.score();
+
+        if (score < 0)
+        {
+            bucketIndex = negBucketCount - 1 - (-score / bucketRange);
+        }
+        else
+        {
+            bucketIndex = negBucketCount + score / bucketRange;
+        }
+        ++count[bucketIndex];
+        maxCount = std::max(maxCount, count[bucketIndex]);
+    }
+
+    distYRel    = (size.y - 20) * 1.0 / maxCount;
+
     if (panelDistanceMap && update)
     {
         panelDistanceMap->Refresh();
